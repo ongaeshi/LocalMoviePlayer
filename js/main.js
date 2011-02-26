c0944a3ad91847d27bf107468feb297cd35cb821
ongaeshi <ongaeshi0621@gmail.com>
2011-02-26 13:47:01 +0900

    htmlもっく

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
 // @author ongaeshi
 // @date   2011/02/26
 
+var TIME_ARRAY = [10, 20, 30, 40, 50];
+
 // ロードイベント
 addEvent(window, "load", function() {
-  MovieLib.setTime(10);
+  MovieLib.setTime(0);
 });
 
 // 記録ボタンをクリック
