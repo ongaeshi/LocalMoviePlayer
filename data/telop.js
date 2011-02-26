4d7f26c06c2f02fde348e1e62f3a2d472061425b
ongaeshi <ongaeshi0621@gmail.com>
2011-02-26 13:38:02 +0900

    いらないデータ削除

--- data/telop.js
+++
@@ -1,7 +0,0 @@
-[
- { 'id':'t1', 'sec': 0.5, 'text':'木曽川 特集', 'fontSize':'48pt', 'top':'50px', 'color':'orange' },
- { 'id':'t2', 'sec': 3, 'text':'木曽川は伊勢湾に流れ込んでいます' },
- { 'id':'t3', 'sec': 6, 'text':'映像は長野県南木曽町で撮影したものです', 'color':'red' },
- { 'id':'t4', 'sec': 9, 'text':'川の色は<span style="color:#8f8">薄緑色</span>で大変綺麗です' },
- { 'id':'t5', 'sec': 13, 'text':'以上で終わりです', 'fontSize':'48pt', 'color':'#0f0' }
-]
