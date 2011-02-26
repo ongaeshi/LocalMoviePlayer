36767ec2cca3a588c8b79bb592c56d019327a0de
ongaeshi <ongaeshi0621@gmail.com>
2011-02-26 11:52:47 +0900

    テスト動画再生

---
+++ js/WebStorage.js
@@ -0,0 +1,70 @@
+//
+// @file 
+// @brief  localStorageを利用した、ページ単位のデータのセーブとロード
+// @author ongaeshi
+// @date   2011/02/25
+
+var WebStorage = (function() {
+  function key() {
+    return document.location.pathname;
+  }
+  
+  function save(object) {
+    JSON.stringify()
+  }
+
+  funtion load() {
+    return 
+  }
+  
+  function get(name) {
+    if (!name)
+      name = key();
+      
+    v = window.localStorage.getItem(name);
+
+    if (v)
+      return parseInt(window.localStorage.getItem(name));
+    else
+      return 0;
+  }
+  
+  function set(v) {
+    window.localStorage.setItem(key(), v);
+  }
+  
+  function _dump() {
+    //console.log(key() + " : " + get());
+  }
+  
+  return {
+    inc: function() {
+      set(get() + 1);
+      _dump();
+    },
+    
+    count: function(name) {
+      return get(name);
+    },
+    
+    clear: function() {
+      window.localStorage.clear();
+      _dump();
+    },
+
+    clearNotify: function(url) {
+      if (confirm("記録をクリアします、よろしいですか？")) {
+        WebStorage.clear();
+
+        if (url)
+          location.replace(url);
+	else
+          location.replace(key());
+      }
+    },
+
+    dump: function() {
+      _dump();
+    }
+  };
+})();
